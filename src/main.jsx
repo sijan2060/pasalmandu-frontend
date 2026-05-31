509169abca03bb020205bb07e02979c70c824a40
Sijan Maharjan <sijanpr07@gmail.com>
2026-05-31 21:27:25 +0545

    Update Message

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./index.css";
 
+import { queryClient } from "./app/queryClient";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
     <App />
     </BrowserRouter>
+    </QueryClientProvider>
+
   </React.StrictMode>
 );
 
