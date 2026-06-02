ea8215d3ea02e68a429702ef1079de3916af08c4
Sijan Maharjan <sijanpr07@gmail.com>
2026-06-02 21:35:36 +0545

    Update Message

---
+++ src/features/products/api/getProducts.js
@@ -0,0 +1,10 @@
+import api from "../../../services/axios";
+
+export const getProducts = async (page = 1) => {
+    const response = await api.get(
+        `/products?page=${page}`
+
+    );
+    return response.data;
+};
+
