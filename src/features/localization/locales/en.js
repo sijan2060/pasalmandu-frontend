14f119e5eedc4ab9cf95a047d611bc191c83cb35
Sijan Maharjan <sijanpr07@gmail.com>
2026-06-03 21:25:24 +0545

    Update Message

---
+++ src/features/localization/locales/en.js
@@ -0,0 +1,17 @@
+export default { 
+    navbar: {
+        home: "Home",
+        products: "Products",
+        cart: "Cart",
+        orders: "Orders",
+    },
+
+    checkout: {
+        title: "Checkout",
+        placeOrder: "Place Order",
+    },
+
+    common: {
+        language: "Language",
+    },
+};
---
+++ src/features/localization/locales/np.js
@@ -0,0 +1,17 @@
+export default {
+  navbar: {
+    home: "गृहपृष्ठ",
+    products: "सामानहरू",
+    cart: "कार्ट",
+    orders: "अर्डरहरू",
+  },
+
+  checkout: {
+    title: "चेकआउट",
+    placeOrder: "अर्डर गर्नुहोस्",
+  },
+
+  common: {
+    language: "भाषा",
+  },
+};
