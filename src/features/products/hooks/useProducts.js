f37f82f05acf3da835b01d2fa12a7f088e3a1e7c
Sijan Maharjan <sijanpr07@gmail.com>
2026-06-07 23:25:58 +0545

    Update Message

--- src/features/products/hooks/useProducts.js
+++ src/features/products/hooks/useProducts.js
@@ -4,6 +4,7 @@ import { getProducts } from "../api/getProducts";
 export const useProducts = (page) => {
   return useQuery({
     queryKey: ["products", page],
+    
     queryFn: () => getProducts(page),
     keepPreviousData: true,
   });
