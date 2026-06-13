afe5c83bbac0302d192a5e3f1da87451e758adb3
Sijan Maharjan <sijanpr07@gmail.com>
2026-06-13 12:25:44 +0545

    Update Message

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,59 +1,60 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./ProtectedRoute";
 
-//Layout for building later
-
-import CustomerLayout from "../layouts/CUstomerLayout";
+import CustomerLayout from "../layouts/CustomerLayout";
 import VendorLayout from "../layouts/VendorLayout";
 import RiderLayout from "../layouts/RiderLayout";
 import AdminLayout from "../layouts/AdminLayout";
 
-// Pages 
+import LoginPage from "../features/auth/pages/LoginPage";
+import NotFound from "../shared/components/NotFound";
 
+// Temporary Pages
 const Home = () => <div>Customer Home</div>;
 const VendorDashboard = () => <div>Vendor Dashboard</div>;
 const RiderDashboard = () => <div>Rider Dashboard</div>;
 const AdminDashboard = () => <div>Admin Dashboard</div>;
-const Login = () => <div>Login Page</div>;
 const Unauthorized = () => <div>Unauthorized Access</div>;
 
 export default function AppRouter() {
-    return (
-        <Routes>
-        
-        {/* Public */} 
-        <Route path="/login" element={<Login />} />
-        <Route path="/unauthorized" element={<Unauthorized />} />
+  return (
+    <Routes>
 
-        {/* Customer */}
-        <Route element={<ProtectedRoute allowedRoles={["customer"]} />}>
-         <Route element={<CustomerLayout />}>
-         <Route path="/" element={<Home />} />
-         </Route>
+      {/* Public Routes */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/unauthorized" element={<Unauthorized />} />
+
+      {/* Customer Routes */}
+      <Route element={<ProtectedRoute allowedRoles={["customer"]} />}>
+        <Route element={<CustomerLayout />}>
+          <Route path="/" element={<Home />} />
         </Route>
-        
-        {/* Vendor */}
-        <Route element={<ProtectedRoute allowedRoles={["vendor"]} />}>
-         <Route element={<VendorLayout />}>
-           <Route path="/vendor" element={<VendorDashboard />} />
-         </Route>
-         </Route>
+      </Route>
 
-         {/* Rider */}
-         <Route element={<ProtectedRoute allowedRoles={["rider"]} />}>
-          <Route element={<RiderLayout />}>
-            <Route path="/rider" element={<RiderDashboard />} />
-          </Route>
-         </Route>
+      {/* Vendor Routes */}
+      <Route element={<ProtectedRoute allowedRoles={["vendor"]} />}>
+        <Route element={<VendorLayout />}>
+          <Route path="/vendor" element={<VendorDashboard />} />
+        </Route>
+      </Route>
 
-         {/* Admin */}
-         <Route element={<ProtectedRoute allowedRoles={["admin", "superadmin"]} />}>
-           <Route element={<AdminLayout />}>
-            <Route path="/admin" element={<AdminDashboard /> } />
-          </Route>
-         </Route>
+      {/* Rider Routes */}
+      <Route element={<ProtectedRoute allowedRoles={["rider"]} />}>
+        <Route element={<RiderLayout />}>
+          <Route path="/rider" element={<RiderDashboard />} />
+        </Route>
+      </Route>
 
+      {/* Admin Routes */}
+      <Route element={<ProtectedRoute allowedRoles={["admin", "superadmin"]} />}>
+        <Route element={<AdminLayout />}>
+          <Route path="/admin" element={<AdminDashboard />} />
+        </Route>
+      </Route>
 
-        </Routes>
-    )
+      {/* 404 */}
+      <Route path="*" element={<NotFound />} />
+
+    </Routes>
+  );
 }
